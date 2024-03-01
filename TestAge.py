9e40d5a115730735846ab788543c98fac7c6e2ab
Maevah <159767837+Maevah01@users.noreply.github.com>
2024-03-01 06:11:08 +0100

    TestAge.py

---
+++ TestAge.py
@@ -0,0 +1,14 @@
+age = int(input("age?"))
+if age<12 :
+    print("enfant")
+elif age<21 :
+    print("adolescent")
+else :
+    print("adulte")
+
+while:
+n=int(input("n="))
+i=1
+while i<=n :
+    print("{} fois".format(i))
+    i=i+1
