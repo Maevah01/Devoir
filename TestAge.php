9942eaa6a7ccfb417df18a47c0e3bddb91210a2c
Maevah <159767837+Maevah01@users.noreply.github.com>
2024-03-01 10:55:26 +0100

    TestAge.php

---
+++ TestAge.php
@@ -0,0 +1,12 @@
+$age = 20;
+function estAdo($age){
+if($age<=18){
+  return true;
+}else{
+  return false;
+if(estAdo($age)){
+  echo "vous etes ado";
+  }else{
+  echo "vous etes majeur";
+  }
+  
